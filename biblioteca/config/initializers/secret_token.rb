e1016490cc064b0f7d7f56e1ee19e780c2e73404
Danilo Soares <daniloluizsb@gmail.com>
2012-11-14 21:49:05 -0200

    Criacao do projeto

---
+++ biblioteca/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblioteca::Application.config.secret_token = '33514c9329d894d3d1c10fe790a59645d41cef3b5703e3cbc06cbae8db5ca0a2d86079974197ed038cdc2a9f5330df9c2a1e3b3d16bb4a499a10f950b9c3ff12'
